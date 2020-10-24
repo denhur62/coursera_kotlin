39818e457ac66e4e780ee2669f4ca88503acd3c3
denhur62 <63595768+denhur62@users.noreply.github.com>
2020-10-24 18:48:30 +0900

    Chapter2 assginment

--- Chapter 2/src/Main.kt
+++ Chapter 2/src/Main.kt
@@ -1,4 +1,10 @@
-
+fun List<Int>.sum(list: List<Int>): Int {
+    var result = 0
+    for (i in list) {
+        result += i
+    }
+    return result
+}
 fun main() {
     println(listOf('a', 'b', 'c').joinToString(
         separator = "", prefix = "(", postfix = ")"))
@@ -8,11 +14,18 @@ fun main() {
     println("Kotlin" in "Java".."Scala")        //true  범위로 알아낼수 있다.
     println("Kotlin" in setOf("Java", "Scala"))     //리스트안에 존재하는 여부를 물어보는것
 
-    val chapter2_1=CheckingIdentifier();
+    val chapter2_1=CheckingIdentifier()
     println(chapter2_1.isValidIdentifier("name"))   // true
     println(chapter2_1.isValidIdentifier("_name"))  // true
     println(chapter2_1.isValidIdentifier("_12"))    // true
     println(chapter2_1.isValidIdentifier(""))       // false
     println(chapter2_1.isValidIdentifier("012"))    // false
     println(chapter2_1.isValidIdentifier("no$"))    // false
+    val sum = listOf(1, 2, 3).sum()
+    println(sum)
+
+    val result = Evaluation(rightPosition = 1, wrongPosition = 1)
+    println( evaluateGuess("BCDF", "ACEB").equals(result))
+    println(evaluateGuess("AAAF", "ABCA").equals(result))
+    println(evaluateGuess("ABCA", "AAAF").equals(result))
 }
